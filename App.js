ee0211ea4c638fa67e8c20056688736399f7779d
sumitsetia <summitsetia@gmail.com>
2023-06-27 23:02:16 +1200

    video covers full screen

--- App.js
+++ App.js
@@ -1,10 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, SafeAreaView, Button } from 'react-native';
+import { Video, ResizeMode } from 'expo-av';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on youu !</Text>
+      {/* <View style={styles.header}>
+        <Text style={styles.headerText}>JPC EXHIBITION EVENT</Text>
+      </View> */}
+      <View style={styles.videoContainer}>
+        <Video
+          source={require('./videos/background.mp4')}
+          style={styles.backgroundVideo}
+          resizeMode={ResizeMode.COVER}
+          shouldPlay
+          isLooping
+        />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +25,34 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'dodgerblue',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center', 
+  },
+  header: {
+    backgroundColor: 'white',
+    padding: 15,
+    flexDirection: 'row',
+    alignItems: 'flex-start',
+    borderRadius: 100,
+    overflow: 'hidden',
+    zIndex: 1,
+  },
+  headerText: {
+    fontSize: 20,
+    zIndex: 1,
+  },
+  videoContainer: {
+    flex: 1,
+    width: '100%',
+    height: '100%', 
+    position: 'relative',
+    zIndex: 0,
+  },
+  backgroundVideo: {
+    flex: 1,
+    width: '100%',
+    height: '100%',
+    zIndex: 0,
   },
 });
